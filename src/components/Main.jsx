870605171950961013281ac0e2c28cf5d7718000
lucasawest <lucasawest1@gmail.com>
2022-10-13 11:53:42 -0600

    third commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {Navbar} from './'
 import {PuppyList} from './'
 
 const Main = () => {
+const [puppyData, setPuppyData] = useState([])
+
+  useEffect(() => {
+    async function getPuppyData(){
+      const response = await fetch ('https://fsa-puppy-bowl.herokuapp.com/2209-FTB-ET-WEB-FT/players')
+      const result = await response.json()
+    }
+    getPuppyData()
+  }, [])
+
   return (
     <div id="main">
     <Navbar/>
-    <PuppyList/>
+    <PuppyList puppyData={puppyData} />
   </div>
   );
 };
--- src/components/PuppyList.js
+++ src/components/PuppyList.js
@@ -12,4 +12,23 @@ import
 
 // }
 
+const AllPuppies = (props) => {
+    const myPuppies = props.puppyData
+
+}
+
+return (
+    <div className='box'>
+    {myPuppies ? myPuppies.map((puppy)=> {
+        return (
+            <div key={`player-${puppy.id}`} className='puppy'>
+                <div>{puppy.name}</div>
+                <div>{puppy.breed}</div>
+                <div>{puppy.status}</div>
+            </div>) })
+        : <div>loading</div>}
+   </div>
+    
+       
+
 export default PuppyList
