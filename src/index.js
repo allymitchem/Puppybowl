39f3966e7e3af34b8adc2322211c3a732ac1d027
lucasawest <lucasawest1@gmail.com>
2022-10-14 08:50:58 -0600

    Comment Out Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React, {useState} from 'react'
 import ReactDOM from "react-dom/client"
 import {Main} from "./components"
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 const container = document.getElementById('app')
 const root = ReactDOM.createRoot(container)
 
+
+// root.render(<React.StrictMode>
+//     <RouterProvider router={router}/>
+// </React.StrictMode>)
 root.render(<Main />)
