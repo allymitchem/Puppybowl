9a65941f43e5a9d00dbb2866c2b8ece13847c296
Allyson Mitchem <allysonmitchem@gmail.com>
2022-10-14 21:42:37 -0600

    added a bit of css

--- src/components/PuppyList.js
+++ src/components/PuppyList.js
@@ -9,20 +9,23 @@ const PuppyList = (props) => {
 console.log(myPuppies)
 
 return (
-    <div className='box'>
+   <div className='box'>
     {myPuppies.length ? myPuppies.map((puppy)=> {
         return (
             <div key={`player-${puppy.id}`} className='puppy'> 
            
-               <span className= "nameId"><div>{puppy.name}</div>
-                <div>{puppy.id}</div></span>
+               <span className= "nameId">
+                <div>{puppy.name}</div>
+                <div><p className="puppyId">#{puppy.id}</p></div></span>
                 <div> 
                     <img className="puppyPic" src = {puppy.imageUrl}
                     alt ="puppyPic"/>
                 </div>
                 {/* <div>{puppy.breed}</div>
                 <div>{puppy.status}</div> */}
+                <span className= "buttons">
                 <button onClick={()=>SetPuppies(puppy)}>See Details</button>
+                </span>
             </div>) })
         : <div>loading</div>}
    
